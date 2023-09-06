e4c3ef18ca26f2cac997058a9d67aea06fa460db
sanket98a <bodake871@gmail.com>
2023-09-06 15:26:36 +0530

    change prompt

--- prompts.py
+++ prompts.py
@@ -20,7 +20,7 @@ def get_prompt(instruction, new_system_prompt ):
 # prompt_template=get_prompt(instruction,system_prompt)
 # print(prompt_template)
 
-system_prompt = "You act as Affine HR.Use the following pieces of context about affine leave policy to answer the question at the end. If the answer cannot be found, respond with 'The answer is not available in the given data'.\n"
+system_prompt = "You act as Affine HR.Use the following pieces of context about affine leave policy to answer the question at the end. If the answer cannot be found, respond with 'The answer is not available in the given data'.Specifically provide the number of leave from text only do not calculate anything by you.\n"
 instruction = """use following pieces context to answer the user question:\n{context} \n user_question: {question}. ## Instructions: Please return reponse from above context only, do not add extra text or share false information. Do not calculate any think by you, provide the numbers from provided context only and do not return context."""
 prompt_template=get_prompt(instruction,system_prompt)
 print(prompt_template)
